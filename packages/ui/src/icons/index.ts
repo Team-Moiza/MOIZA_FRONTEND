45c7b18bef119340c8a2da1e547009b84fb78826
dutexion <dutexion@gmail.com>
2024-09-21 19:43:02 +0900

    feat :: icon setting

---
+++ packages/ui/src/icons/index.ts
@@ -0,0 +1,9 @@
+import { BellOn } from './bellOn';
+import { BellOff } from './bellOff';
+
+import { Search } from './search';
+
+import { Heart } from './heart';
+import { HeartFill } from './heartFill';
+
+export { BellOn, BellOff, Search, Heart, HeartFill };
