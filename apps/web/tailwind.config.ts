3602f7629239c1a1082ca52613437d98c85ff9f9
dutexion <dutexion@gmail.com>
2024-09-22 17:57:42 +0900

    setting :: tailwind setting

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,14 @@
+const sharedConfig = require('@repo/tailwind-config');
+
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  presets: [sharedConfig],
+  plugins: [],
+};
