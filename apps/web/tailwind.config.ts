bb0dda5c9de281139a71e493703bdd6f1f1c9b96
dutexion <dutexion@gmail.com>
2024-09-22 22:43:49 +0900

    setting :: font setting

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -5,11 +5,16 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-
-    // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx,mdx}',
     '../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   presets: [sharedConfig],
   plugins: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        pretendard: ['var(--font-pretendard)'],
+      },
+    },
+  },
 };
