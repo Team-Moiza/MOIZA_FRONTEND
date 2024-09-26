4c61a22ca73b11ab6ceedb542dbb848b7e6cae9a
rkdalswl718 <rkdalswl070718@gmail.com>
2024-09-26 12:57:58 +0900

    style :: 좌우 버블 배치

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 import React from "react";
-import { Assets } from "@repo/ui/src";
 import { Icon } from "@repo/ui/src";
+import { Assets } from "@repo/ui/src";
 
 const Login = () => {
   return (
-    <div className="h-screen w-screen overflow-hidden">
-      <div className="flex flex-col justify-center items-center h-full bg-background">
-        <div className="gap-[30px] flex flex-col items-center mb-10">
+    <div className="h-screen w-screen overflow-hidden relative bg-background pt-[200px]">
+      <div className="flex flex-col items-center h-full z-10">
+        <div className="gap-[30px]">
           <div
-            className="relative bg-white px-[18px] py-[10px] rounded-full text-center text-p1"
+            className="relative bg-white px-[16px] py-[10px] rounded-full text-center text-p1 mb-[30px] "
             style={{
               boxShadow: "0px 2px 12px rgba(171, 190, 209, 0.25)",
             }}
@@ -17,23 +17,40 @@ const Login = () => {
             소마생들 다 여기 모여라! 🚀
             <div className="absolute bottom-[-8px] left-1/2 transform -translate-x-1/2 w-4 h-4 bg-white rotate-45"></div>
           </div>
-          <Icon.Logo size={258} />
-          <div className="text-subText1 text-[20px]">4대 소마고 연합 커뮤니티 모이자</div>
+          <div className="flex flex-col items-center gap-[15px]">
+            <Icon.Logo size={265} />
+            <div className="text-subText1 text-[20px]">
+              4대 소마고 연합 커뮤니티 모이자
+            </div>
+          </div>
         </div>
-        <div className="relative">
-          <button className="flex w-[360px] h-[48px] bg-white rounded-lg justify-center items-center"
-          style={{
-            boxShadow: "0px 2px 12px rgba(171, 190, 209, 0.25)",
-          }}>
-            <div className="absolute left-4">
+      </div>
+      <div className="absolute left-[70px] top-1/2 transform -translate-y-1/2">
+        <Assets.Bubble />
+      </div>
+      <div className="absolute right-[70px] top-1/2 transform -translate-y-1/2 scale-x-[-1]">
+        <Assets.Bubble />
+      </div>
+
+      <div className="absolute left-1/2 top-[420px] transform -translate-x-1/2 bg-primary-400 w-[115vw] h-[100vw] rounded-[50%]">
+        <div className="flex flex-col justify-start items-center h-full pt-[5%]">
+          <button
+            className="relative flex bg-white rounded-lg justify-center items-center "
+            style={{
+              boxShadow: "0px 2px 12px rgba(171, 190, 209, 0.25)",
+            }}
+          >
+            <div className="absolute left-5">
               <Icon.Google />
             </div>
-            <span className="text-center text-bnt1">구글로 로그인</span>
+            <span className="text-center text-bnt1 px-[100px] py-3">
+              구글로 로그인
+            </span>
           </button>
-        </div>
-        <div className="text-p2 text-accent">
-          dsm.hs.kr, gsm.hs.kr, dgws.hs.kr, bssm.hs.kr 이메일만 로그인 할 수
-          있습니다.
+          <div className="text-accent text-sm mt-[50px]">
+            dsm.hs.kr, gsm.hs.kr, dgws.hs.kr, bssm.hs.kr 이메일만 로그인 할 수
+            있습니다.
+          </div>
         </div>
       </div>
     </div>
--- packages/ui/src/assets/Bubble.tsx
+++ packages/ui/src/assets/Bubble.tsx
@@ -1,46 +1,15 @@
 export const Bubble = () => {
   return (
     <svg
-      width="100%"
-      height="100%"
-      viewBox="0 0 1510 574"  // viewBox 수정
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      style={{ display: 'block', margin: 0, padding: 0, overflow: 'hidden' }}
+      width="177"
+      height="190"
+      viewBox="0 0 177 190"
+      fill="none"
     >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M1509.98 574H0C113.305 266.944 408.579 48 754.99 48C1101.4 48 1396.67 266.944 1509.98 574Z"
-        fill="#47A9FF"
-      />
-      <circle opacity="0.25" cx="266" cy="110" r="16" fill="#47A9FF" />
-      <circle opacity="0.75" cx="145" cy="24" r="24" fill="#47A9FF" />
-      <circle opacity="0.5" cx="145" cy="150" r="40" fill="#47A9FF" />
-      <circle
-        opacity="0.25"
-        cx="16"
-        cy="16"
-        r="16"
-        transform="matrix(-1 0 0 1 1260 94)"
-        fill="#47A9FF"
-      />
-      <circle
-        opacity="0.75"
-        cx="24"
-        cy="24"
-        r="24"
-        transform="matrix(-1 0 0 1 1389 0)"
-        fill="#47A9FF"
-      />
-      <circle
-        opacity="0.5"
-        cx="40"
-        cy="40"
-        r="40"
-        transform="matrix(-1 0 0 1 1405 110)"
-        fill="#47A9FF"
-      />
+      <circle opacity="0.25" cx="161" cy="110" r="16" fill="#47A9FF" />
+      <circle opacity="0.75" cx="40" cy="24" r="24" fill="#47A9FF" />
+      <circle opacity="0.5" cx="40" cy="150" r="40" fill="#47A9FF" />
     </svg>
   );
 };
