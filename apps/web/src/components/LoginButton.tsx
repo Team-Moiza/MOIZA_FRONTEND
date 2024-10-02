ea5e5f96f3b36913f3081471ef19ed3a46f79df5
rkdalswl718 <rkdalswl070718@gmail.com>
2024-10-03 00:39:24 +0900

    feat :: 로그인 링크 받아오기

--- apps/web/src/components/LoginButton.tsx
+++ apps/web/src/components/LoginButton.tsx
@@ -1,12 +1,23 @@
 import { Icon } from "@repo/ui/src";
+import axios from "axios";
+import { customAxios } from "../utils/customAxios";
 
 export const LoginButton = () => {
+  const onAuthClick = async() => {
+    try {
+      const res = await customAxios.get("/auth")
+      window.location.href=res.data
+    } catch(error) {
+      console.log(error)
+    }
+  }
   return (
     <button
       className="flex bg-white rounded-lg justify-center items-center px-[50px] py-[14px] gap-[20px]"
       style={{
         boxShadow: "0px 2px 12px rgba(171, 190, 209, 0.25)",
       }}
+      onClick={onAuthClick}
     >
         <Icon.Google />
       <div className="text-center text-p2">
