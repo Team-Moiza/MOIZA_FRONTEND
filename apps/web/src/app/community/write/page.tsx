76423a055403bbef1c269e4a46fe59172a49e9f0
dutexion <dutexion@gmail.com>
2024-10-09 21:10:54 +0900

    style :: text-editor

--- apps/web/src/app/community/write/page.tsx
+++ apps/web/src/app/community/write/page.tsx
@@ -1,7 +1,18 @@
 'use client';
 import React, { useState } from 'react';
+import { Icon } from '@repo/ui/src';
 
 type PostType = 'post' | 'survey';
+
+const EditedArray = [
+  Icon.Bold,
+  Icon.Italic,
+  Icon.LineThrough,
+  Icon.Highlight,
+  Icon.Quotes,
+  Icon.Link,
+  Icon.Code,
+];
 
 export default function CommunityWrite() {
   const [selectedOption, setSelectedOption] = useState<PostType>('post');
@@ -12,9 +23,9 @@ export default function CommunityWrite() {
 
   return (
     <div className="bg-background h-[calc(100vh_-_80px)] flex flex-col gap-[20px] pt-[142px] items-center">
-      <div className="w-[936px] flex flex-col">
+      <div className="w-[936px] flex flex-col gap-[12px]">
         <div className="flex gap-[20px] w-[100%] px-[8px]">
-          <label className="flex gap-[8px] items-center">
+          <label className="flex gap-[8px] items-center text-btn3">
             <input
               type="radio"
               value="post"
@@ -29,7 +40,7 @@ export default function CommunityWrite() {
             </div>
             게시물
           </label>
-          <label className="flex gap-[8px] items-center">
+          <label className="flex gap-[8px] items-center text-btn3">
             <input
               type="radio"
               value="survey"
@@ -44,6 +55,34 @@ export default function CommunityWrite() {
             </div>
             설문
           </label>
+        </div>
+        <input
+          type="text"
+          className="w-[100%] h-[52px] px-[20px] rounded-[4px] outline-none border-accent border-[1px] text-text placeholder-subText2 text-p1"
+          placeholder="제목을 입력해주세요"
+        />
+        <div className="flex gap-[8px] w-[100%]">
+          <input
+            className="px-[12px] w-[94px] h-[30px] flex justify-center items-center bg-[rgba(18,18,18,0.04)] placeholder-subText1 text-btn2 rounded-[4px] outline-none"
+            placeholder="#태그 입력"
+          />
+        </div>
+        <div className="w-[100%] mt-[2px] border-[1px] border-accent min-h-[272px] rounded-[4px] bg-white relative">
+          <div className="w-[100%] h-[40px] border-b-[1px] border-accent flex items-center gap-[8px] px-[12px]">
+            {EditedArray.map((icon, index) => {
+              return (
+                <React.Fragment key={index}>
+                  {icon()}
+                  {index !== EditedArray.length && (
+                    <div className="border-[1px] border-accent h-[24px] w-[0]" />
+                  )}
+                </React.Fragment>
+              );
+            })}
+          </div>
+          <textarea
+            placeholder="내용을 입력해주세요"
+            className="outline-none w-[100%] min-h-[100%] resize-none p-[20px] placeholder-subText2 text-text text-p1"></textarea>
         </div>
       </div>
     </div>
