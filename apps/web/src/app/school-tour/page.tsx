825a8f722f8e3be7ac18f344b03dd453e23e4041
rkdalswl718 <rkdalswl070718@gmail.com>
2024-10-14 00:51:37 +0900

    add :: title

---
+++ apps/web/src/app/school-tour/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SchoolTour = () => {
+  return (
+    <div className="bg-background h-[calc(100vh_-_80px)] flex flex-col gap-[20px] pt-[142px] items-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-2">SW마이스터고</h1>
+        <p className="text-gray-500">소프트웨어 인재를 키우는 4개의 학교, 각 학교의 이야기를 확인해보세요.</p>
+      </div>
+
+
+    </div>
+  );
+};
+
+export default SchoolTour;
