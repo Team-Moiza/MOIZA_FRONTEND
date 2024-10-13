dfb01aa2df6196c46e88c6d49976af18fe252fb7
rkdalswl718 <rkdalswl070718@gmail.com>
2024-10-14 02:53:29 +0900

    add :: map돌림

--- apps/web/src/app/school-tour/page.tsx
+++ apps/web/src/app/school-tour/page.tsx
@@ -1,30 +1,31 @@
-import { Assets } from '@repo/ui/src'; // Assets에서 이미지들을 import
+import { Assets } from '@repo/ui/src';
 
 const SchoolTour = () => {
+  const schoolLogo = [
+    { logo: Assets.GSM, name: '광주SW마이스터고등학교' },
+    { logo: Assets.DGSW, name: '대구SW마이스터고등학교' },
+    { logo: Assets.DSM, name: '대덕SW마이스터고등학교' },
+    { logo: Assets.BSM, name: '부산SW마이스터고등학교' },
+  ];
+
   return (
     <div className="bg-background h-[calc(100vh_-_80px)] flex flex-col items-center">
-      <div className="text-center">
+      <div className="pt-[180px] w-[936px]">
+        <div className="flex flex-col items-center">
         <div className="text-H1 text-text">SW마이스터고</div>
         <div className="text-subText1">소프트웨어 인재를 키우는 4개의 학교, 각 학교의 이야기를 확인해보세요.</div>
+        </div>
+      <div className="flex flex-row w-full justify-between mt-[50px]">
+        {schoolLogo.map((school, index) => (
+          <div key={index} className="flex flex-col items-center gap-[30px]">
+            <div key={index} className="w-[220px] h-[300px] bg-white shadow-shadow rounded-[12px] flex flex-col items-center justify-center">
+              <img src={school.logo.src} className="w-[80%]" />
+            </div>
+              <p className="text-p3 text-text">{school.name}</p>
+          </div>
+        ))}
       </div>
-      <div className="grid grid-cols-4 gap-[20px] mt-[40px]">
-        <div className="bg-white p-6 rounded-[12px] shadow-md flex flex-col items-center">
-          <img src={Assets.GSM.src} alt="GSM 로고" className="h-[80px] mb-4" />
-          <p className="text-lg font-semibold">광주SW마이스터고등학교</p>
         </div>
-        <div className="bg-white p-6 rounded-[12px] shadow-md flex flex-col items-center">
-          <img src={Assets.DGSW.src} alt="DGSW 로고" className="h-[80px] mb-4" />
-          <p className="text-lg font-semibold">대구SW마이스터고등학교</p>
-        </div>
-        <div className="bg-white p-6 rounded-[12px] shadow-md flex flex-col items-center">
-          <img src={Assets.DSM.src} alt="DSM 로고" className="h-[80px] mb-4" />
-          <p className="text-lg font-semibold">대덕SW마이스터고등학교</p>
-        </div>
-        <div className="bg-white p-6 rounded-[12px] shadow-md flex flex-col items-center">
-          <img src={Assets.BSM.src} alt="BSM 로고" className="h-[80px] mb-4" />
-          <p className="text-lg font-semibold">부산SW마이스터고등학교</p>
-        </div>
-      </div>
     </div>
   );
 };
