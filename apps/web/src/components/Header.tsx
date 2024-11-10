53b9b8b3b6550f50a6a883ad992898bb58dd0f68
dutexion <dutexion@gmail.com>
2024-11-11 08:38:29 +0900

    fix :: dependency error

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import * as Assets from '@repo/ui/src'
+import * as Assets from '@moija/ui';
 
 type User = {
   profile: string;
@@ -8,34 +8,35 @@ type User = {
 const Header = () => {
   const isLoggedIn = true;
   const user: User = {
-    profile: 'https://i.pinimg.com/564x/b3/ea/18/b3ea1834562fc0dcbe9a7f3c4ef7612b.jpg',
-    name: '강민지'
+    profile:
+      'https://i.pinimg.com/564x/b3/ea/18/b3ea1834562fc0dcbe9a7f3c4ef7612b.jpg',
+    name: '강민지',
   };
 
   return (
     <header className="w-[100vw] fixed justify-center bg-white h-[80px] border-b-[1px] border-gray-100">
-    <div className="mx-[200px] h-full flex justify-between items-center">
-      <div className="flex items-center space-x-4">
-        <Assets.Logo />
+      <div className="mx-[200px] h-full flex justify-between items-center">
+        <div className="flex items-center space-x-4">
+          <Assets.Logo />
+        </div>
+        <div className="flex items-center gap-5">
+          {isLoggedIn ? (
+            <div className="flex items-center gap-[14px]">
+              <img
+                src={user.profile}
+                className="rounded-full w-[42px] h-[42px] flex-shrink-0"
+              />
+              <div className="text-p3 text-black">{user.name}</div>
+            </div>
+          ) : (
+            <div className="gap-[50px] flex">
+              <div className="text-p3 text-black">로그인</div>
+              <div className="text-p3 text-black">회원가입</div>
+            </div>
+          )}
+        </div>
       </div>
-      <div className="flex items-center gap-5">
-        {isLoggedIn ? (
-          <div className="flex items-center gap-[14px]">
-            <img
-              src={user.profile}
-              className="rounded-full w-[42px] h-[42px] flex-shrink-0"
-            />
-            <div className="text-p3 text-black">{user.name}</div>
-          </div>
-        ) : (
-          <div className="gap-[50px] flex">
-          <div className="text-p3 text-black">로그인</div>
-          <div className="text-p3 text-black">회원가입</div>
-          </div>
-        )}
-      </div>
-    </div>
-  </header>
+    </header>
   );
 };
 
