289265d2db78b4c9af8bfc2c23e168321b0801a1
dutexion <dutexion@gmail.com>
2024-11-11 10:23:55 +0900

    refactor :: center component

--- packages/ui/src/components/layouts/Center.tsx
+++ packages/ui/src/components/layouts/Center.tsx
@@ -1,10 +1,17 @@
 type CenterPropsType = {
   children: React.ReactNode;
+  horizontal?: boolean;
+  vertical?: boolean;
 };
 
-export const Center = ({ children }: CenterPropsType) => {
+export const Center = ({
+  children,
+  horizontal = true,
+  vertical = true,
+}: CenterPropsType) => {
   return (
-    <div className="w-full h-full flex justify-center items-center">
+    <div
+      className={`w-full h-full flex ${horizontal && 'justify-center'} ${vertical && 'items-center'}`}>
       <div className="w-fit h-fit">{children}</div>
     </div>
   );
