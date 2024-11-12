b3e0b3b3cfb63e6a5d42358e84ff212c43700c2e
rkdalswl718 <rkdalswl070718@gmail.com>
2024-11-12 14:21:27 +0900

    add :: 상단카테고리

---
+++ apps/web/src/app/portfolio-list/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React, { useState } from "react";
+
+const PortfolioList = () => {
+    const categories = [
+        "전체",
+        "프론트엔드 개발자",
+        "백엔드 개발자",
+        "UXUI디자이너",
+        "기획자",
+    ];
+    const [selectedCategory, setSelectedCategory] = useState("전체");
+    return (
+        <div className="h-screen w-screen pt-[150px] px-[200px]">
+            <div className="flex gap-2 mb-[30px]">
+                {categories.map((category) => (
+                    <button
+                        key={category}
+                        onClick={() => setSelectedCategory(category)}
+                        className={`px-4 py-2 rounded-full ${
+                            selectedCategory === category
+                                ? "bg-primary-500 text-white"
+                                : "bg-white text-gray-600 border-[1px] border-gray-200"
+                        }`}
+                    >
+                        {category}
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default PortfolioList;
