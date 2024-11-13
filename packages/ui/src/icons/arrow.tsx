0798fe957f93fb6f35f98d8bc30dfb114235c703
rkdalswl718 <rkdalswl070718@gmail.com>
2024-11-13 09:33:38 +0900

    chore :: arrow props

--- packages/ui/src/icons/arrow.tsx
+++ packages/ui/src/icons/arrow.tsx
@@ -1,4 +1,8 @@
-export const Arrow = () => {
+interface ArrowProps {
+    isOpen?: boolean;
+}
+
+export const Arrow = ({ isOpen }: ArrowProps) => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -6,13 +10,14 @@ export const Arrow = () => {
             height="14"
             viewBox="0 0 14 14"
             fill="none"
+            style={{ transform: isOpen ? "rotate(180deg)" : "rotate(0deg)", transition: "transform 0.3s" }}
         >
             <path
                 d="M11.0833 4.95825L6.99996 9.04159L2.91663 4.95825"
                 stroke="#5A5A5A"
-                stroke-width="0.875"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="0.875"
+                strokeLinecap="round"
+                strokeLinejoin="round"
             />
         </svg>
     );
