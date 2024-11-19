cf021263062e38c5d48764258d6c09e75a34c9e2
dutexion <dutexion@gmail.com>
2024-11-19 13:58:52 +0900

    style :: edit input & textarea

--- packages/ui/src/components/Input/index.tsx
+++ packages/ui/src/components/Input/index.tsx
@@ -1,11 +1,13 @@
-interface InputPropsType extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface InputPropsType extends React.InputHTMLAttributes<HTMLInputElement> {
+  isBig?: boolean;
+}
 
-export const Input = ({ ...rest }: InputPropsType) => {
+export const Input = ({ isBig, ...rest }: InputPropsType) => {
   return (
     <input
       {...rest}
-      style={{ width: rest.width + 'px' }}
-      className="h-[48px] px-[16px] border border-gray-200 rounded-[8px] text-black text-p5 placeholder:text-gray-400 outline-0"
+      style={{ width: rest.width + 'px', height: isBig ? '60px' : '48px' }}
+      className={`px-[16px] border border-gray-200 rounded-[8px] text-black text-${isBig ? 'p1' : 'p5'} placeholder:text-gray-400 outline-0`}
     />
   );
 };
--- packages/ui/src/components/Textarea/index.tsx
+++ packages/ui/src/components/Textarea/index.tsx
@@ -2,14 +2,20 @@ interface TextareaPropsType
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   width: number;
   height: number;
+  isBig?: boolean;
 }
 
-export const Textarea = ({ width, height, ...rest }: TextareaPropsType) => {
+export const Textarea = ({
+  width,
+  height,
+  isBig = false,
+  ...rest
+}: TextareaPropsType) => {
   return (
     <textarea
       {...rest}
       style={{ width: width + 'px', height: height + 'px' }}
-      className="h-[48px] px-[24px] py-[16px] border border-gray-200 rounded-[8px] text-black text-p5 placeholder:text-gray-400 outline-0 resize-none"
+      className={`h-[48px] px-[24px] py-[16px] border border-gray-200 rounded-[8px] text-black text-${isBig ? 'p1' : 'p5'} placeholder:text-gray-400 outline-0 resize-none`}
     />
   );
 };
