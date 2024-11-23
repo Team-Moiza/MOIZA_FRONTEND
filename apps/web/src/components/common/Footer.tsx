b30048283d25ebe487d830ac6d6f0930c2ea9184
rkdalswl718 <rkdalswl070718@gmail.com>
2024-11-23 15:05:13 +0900

    style :: 깃허브 아이콘 추가

--- apps/web/src/components/common/Footer.tsx
+++ apps/web/src/components/common/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Footer = () => {
     return (
-        <footer className="bottom-0 mt-[120px] w-full h-[380px] px-[200px] bg-gray-600 flex flex-col items-center justify-center text-white">
+        <footer className="relative bottom-0 mt-[100px] w-full h-[380px] px-[200px] bg-gray-600 flex flex-col items-center justify-center text-white">
             <div className="font-black text-[22px] mb-6">MOIZA</div>
             <div className="flex gap-4 mb-6 text-caption1 text-white">
                 {["강민지", "김명진", "김수아", "안혜성", "이지후"].map(
@@ -19,7 +19,7 @@ const Footer = () => {
             <div className="text-[13px] text-gray-300 flex items-center mt-[30px]">
                 ©2024. team MOIZA. All rights reserved.
             </div>
-            <div className="absolute bottom-0 right-0">
+            <div className="absolute top-[240px] right-[200px] ">
                 <Link href="https://github.com/Team-Moiza">
                     <Github />
                 </Link>
