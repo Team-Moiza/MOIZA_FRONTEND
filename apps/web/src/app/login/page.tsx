6fdb839fdc1696d42a956b945f6033ec2d7122eb
rkdalswl718 <rkdalswl070718@gmail.com>
2024-12-11 17:23:01 +0900

    add :: 로그인 페이지 퍼블리싱

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,23 +1,25 @@
 import { Google, Logo } from "@moija/ui";
-import React from "react";
 
 const Login = () => {
     return (
-        <div className="w-screen h-screen items-center justify-center bg-gray-50 flex flex-col">
-            <div className="w-[500px] h-[500px] gap-[50px] flex-col justify-center items-center  bg-white border border-gray-100 rounded-[30px]">
-                <div className="gap-5 items-center flex flex-col ">
-                    <Logo width={150} />
-                    <div className="text-p2 text-gray-600">
-                        로그인하고 소마고생들의 포트폴리오를 구경해보세요!
+        <div className="w-screen h-screen flex items-center justify-center bg-gray-50">
+            <div className="flex flex-col items-center">
+                <div className="w-[500px] h-[480px] flex flex-col items-center justify-center gap-[50px] bg-white border border-gray-100 rounded-[30px]">
+                    <div className="flex flex-col items-center gap-5">
+                        <Logo width={150} />
+                        <div className="text-gray-600 text-center text-p4">
+                            로그인하고 소마고생들의 포트폴리오를 구경해보세요!
+                        </div>
                     </div>
-                    <button className="flex rounded-[50px] border-2 border-[#E2E8F0] px-6 py-3 gap-4 text-black text-p2 ">
-                        <Google />
-                        Google로 로그인
-                    </button>
+                        <button className="flex items-center gap-4 border-2 border-[#E2E8F0] px-10 py-4 rounded-[50px] text-black">
+                            <Google />
+                            Google로 로그인
+                        </button>
                 </div>
-            </div>
-            <div className="text-p3 mt-[50px] text-gray-300">
-                ©2024. team MOIZA. All rights reserved.
+                {/* 하단 텍스트 */}
+                <div className="text-gray-300 text-caption1 mt-[50px]">
+                    ©2024. team MOIZA. All rights reserved.
+                </div>
             </div>
         </div>
     );
