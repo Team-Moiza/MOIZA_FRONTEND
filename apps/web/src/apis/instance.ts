a09ce4c0e95cf79732b804484882f237e6227ea7
rkdalswl718 <rkdalswl070718@gmail.com>
2025-03-17 06:31:25 +0900

    fix :: refresh toekn 무한요청 방지

--- apps/web/src/apis/instance.ts
+++ apps/web/src/apis/instance.ts
@@ -7,28 +7,36 @@ export const instance = axios.create({
 
 instance.interceptors.request.use((config) => {
     const token = localStorage.getItem("accessToken");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
+    if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
 });
+
 instance.interceptors.response.use(
     (res) => res,
     async (err) => {
-        if (err.response?.status !== 401) {
+        const originalRequest = err.config;
+
+        if (originalRequest.url === "/auth/refresh") {
+            localStorage.removeItem("accessToken");
+            localStorage.removeItem("refreshToken");
+            window.location.replace("/login");
             return Promise.reject(err);
         }
 
+        if (err.response?.status !== 401) return Promise.reject(err);
+
         try {
+            originalRequest._retry = true;
             const refreshToken = localStorage.getItem("refreshToken");
-            const { data } = await instance.post("/auth/refresh", {
+            const { data } = await axios.post("/auth/refresh", {
                 token: refreshToken,
             });
 
             localStorage.setItem("accessToken", data.accessToken);
-            err.config.headers.Authorization = `Bearer ${data.accessToken}`;
+            instance.defaults.headers.common["Authorization"] =
+                `Bearer ${data.accessToken}`;
 
-            return instance(err.config);
+            return instance(originalRequest);
         } catch (error) {
             localStorage.removeItem("accessToken");
             localStorage.removeItem("refreshToken");
