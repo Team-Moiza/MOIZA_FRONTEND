6d1a15c6c709578adf6b208eff2b5a7dd8baefa9
rkdalswl718 <rkdalswl070718@gmail.com>
2025-03-20 14:49:51 +0900

    add :: favicon 추가

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import Header from "../components/layouts/Header";
 import "./globals.css";
 import localFont from "next/font/local";
 import { Provider } from "./Provider";
+import { Metadata } from "next";
 
 const pretendard = localFont({
   src: "../../public/PretendardVariable.woff2",
@@ -9,6 +10,14 @@ const pretendard = localFont({
   weight: "45 920",
   variable: "--font-pretendard",
 });
+
+export const metadata: Metadata = {
+	title: "모이자",
+	description: "4대 소마고 이력서 공유 사이트",
+	icons: {
+		icon: "Moiza.jpg",
+	},
+};
 
 export default function RootLayout({
   children,
