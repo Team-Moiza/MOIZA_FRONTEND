c182682078f971595f6c6b8412d8e6f9ae782511
rkdalswl718 <rkdalswl070718@gmail.com>
2025-04-01 01:51:48 +0900

    fix :: my page 오류

--- apps/web/src/app/my/page.tsx
+++ apps/web/src/app/my/page.tsx
@@ -1,100 +1,86 @@
 "use client";
 
-import { BottomArrow, Logo } from "@moija/ui";
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
+import { Dialog } from "./Dialog";
+import { useState } from "react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+    logout,
+    removeAccount,
+    deletePortFolio,
+    myPortFolio,
+} from "../../apis";
 import { useRouter } from "next/navigation";
-import { ActionMenu } from "@moija/ui";
-import { user, logout } from "../../apis/user";
-import { instance } from "../../apis";
-import { useMutation } from "@tanstack/react-query";
-import cookies from "js-cookie";
+import { toast } from "react-toastify";
+import { ProfilePage } from "./ProfilePage";
+import { LikedList } from "./LikedList";
+import { MyPageNav } from "./MypageNav";
+import { Center } from "@moija/ui";
 
-type User = {
-    profile: string;
-    nickname: string;
-};
+export default function MyPage() {
+    const [activeTab, setActiveTab] = useState<"profile" | "liked">("profile");
+    const [type, setType] = useState<
+        null | `removeResume_${string}` | "removeAccount"
+    >(null);
+    const { replace } = useRouter();
 
-const Header = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [userData, setUserData] = useState<User | null>(null);
-
-    const router = useRouter();
-    useEffect(() => {
-        const fetchUserProfile = async () => {
-            const token = cookies.get("accessToken");
-            if (token) {
-                const response = await instance.get("/users");
-                setUserData(response.data);
-                setIsLoggedIn(true);
-            }
-        };
-        fetchUserProfile();
-    }, []);
+    const { mutate: removeResume } = useMutation({
+        mutationFn: deletePortFolio,
+        onSuccess: () => {
+            toast.success("이력서를 성공적으로 제거하였습니다!");
+            refetch();
+        },
+    });
 
     const { mutate: userLogout } = useMutation({
         mutationFn: logout,
         onSuccess: () => {
-            cookies.remove("accessToken");
-            cookies.remove("refreshToken");
-            setUserData(null);
-            router.replace("/login");
+            localStorage.removeItem("accessToken");
+            localStorage.removeItem("refreshToken");
+            replace("/login");
         },
     });
 
-    return (
-        <header className="z-[100] w-[100vw] fixed justify-center bg-white h-[80px] px-[200px] py-[25px] shadow-custom">
-            <div className="h-full flex justify-between items-center">
-                <div className="flex items-center space-x-4">
-                    <Link href="/">
-                        <Logo />
-                    </Link>
-                </div>
-                <div className="flex items-center gap-5">
-                    {isLoggedIn && userData ? (
-                        <ActionMenu
-                            items={[
-                                {
-                                    label: "프로필 보기",
-                                    onClick: () => router.push("/my"),
-                                },
-                                {
-                                    label: "이력서 목록",
-                                    onClick: () => router.push("/"),
-                                },
-                                {
-                                    label: "로그아웃",
-                                    onClick: userLogout,
-                                    fontColor: "text-red-500",
-                                },
-                            ]}
-                        >
-                            <div className="flex items-center gap-3 cursor-pointer">
-                                <Image
-                                    src={userData.profile}
-                                    alt="프로필"
-                                    width={42}
-                                    height={42}
-                                    className="rounded-full flex-shrink-0"
-                                />
-                                <div className="flex items-center gap-1.5">
-                                    <div className="text-p3 text-black">
-                                        {userData.nickname}님
-                                    </div>
-                                    <BottomArrow size="18" />
-                                </div>
-                            </div>
-                        </ActionMenu>
-                    ) : (
-                        <Link href="/login" className="gap-[50px] flex">
-                            <div className="text-p3 text-black">로그인</div>
-                        </Link>
-                    )}
-                </div>
-            </div>
-        </header>
-    );
-};
+    const { mutate: removeA } = useMutation({
+        mutationFn: removeAccount,
+        onSuccess: () => {
+            localStorage.removeItem("accessToken");
+            localStorage.removeItem("refreshToken");
+            replace("/");
+        },
+    });
 
-export default Header;
+    const { data, refetch } = useQuery({
+        queryKey: ["my", "portfolio"],
+        queryFn: myPortFolio,
+    });
+
+    return (
+        <>
+            <Dialog
+                type={type}
+                setType={setType}
+                removeAccount={removeA}
+                removeResume={(resumeId: string) => removeResume(resumeId)}
+                refetch={refetch}
+            />
+
+            <Center>
+                <div className="pt-[80px] w-[1040px] h-screen flex gap-[10px]">
+                    <MyPageNav
+                        activeTab={activeTab}
+                        setActiveTab={setActiveTab}
+                        userLogout={userLogout}
+                        setType={setType}
+                    />
+                    <div className="w-full mt-10 flex flex-col gap-6">
+                        {activeTab === "profile" ? (
+                            <ProfilePage setType={setType} />
+                        ) : (
+                            <LikedList />
+                        )}
+                    </div>
+                </div>
+            </Center>
+        </>
+    );
+}
